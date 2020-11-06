997e58c9f619cde6ecd8b68ca54f1e73a4a35a79
rob <rmillikin@wisc.edu>
2020-11-06 11:10:40 -0600

    clone metamorpheus

---
+++ metamorpheus_module/EngineLayer/ClassicSearch/ClassicSearchEngine.cs
@@ -0,0 +1,168 @@
+﻿using MzLibUtil;
+using Proteomics;
+using Proteomics.Fragmentation;
+using Proteomics.ProteolyticDigestion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EngineLayer.ClassicSearch
+{
+    public class ClassicSearchEngine : MetaMorpheusEngine
+    {
+        private readonly MassDiffAcceptor SearchMode;
+        private readonly List<Protein> Proteins;
+        private readonly List<Modification> FixedModifications;
+        private readonly List<Modification> VariableModifications;
+        private readonly List<SilacLabel> SilacLabels;
+        private readonly (SilacLabel StartLabel, SilacLabel EndLabel)? TurnoverLabels;
+        private readonly PeptideSpectralMatch[] PeptideSpectralMatches;
+        private readonly Ms2ScanWithSpecificMass[] ArrayOfSortedMS2Scans;
+        private readonly double[] MyScanPrecursorMasses;
+
+        public ClassicSearchEngine(PeptideSpectralMatch[] globalPsms, Ms2ScanWithSpecificMass[] arrayOfSortedMS2Scans,
+            List<Modification> variableModifications, List<Modification> fixedModifications, List<SilacLabel> silacLabels, SilacLabel startLabel, SilacLabel endLabel, 
+            List<Protein> proteinList, MassDiffAcceptor searchMode, CommonParameters commonParameters, List<(string FileName, CommonParameters Parameters)> fileSpecificParameters, List<string> nestedIds)
+            : base(commonParameters, fileSpecificParameters, nestedIds)
+        {
+            PeptideSpectralMatches = globalPsms;
+            ArrayOfSortedMS2Scans = arrayOfSortedMS2Scans;
+            MyScanPrecursorMasses = arrayOfSortedMS2Scans.Select(b => b.PrecursorMass).ToArray();
+            VariableModifications = variableModifications;
+            FixedModifications = fixedModifications;
+            SilacLabels = silacLabels;
+            if (startLabel != null || endLabel != null) //else it's null
+            {
+                TurnoverLabels = (startLabel, endLabel);
+            }
+            Proteins = proteinList;
+            SearchMode = searchMode;
+        }
+
+        protected override MetaMorpheusEngineResults RunSpecific()
+        {
+            Status("Getting ms2 scans...");
+
+            double proteinsSearched = 0;
+            int oldPercentProgress = 0;
+
+            // one lock for each MS2 scan; a scan can only be accessed by one thread at a time
+            var myLocks = new object[PeptideSpectralMatches.Length];
+            for (int i = 0; i < myLocks.Length; i++)
+            {
+                myLocks[i] = new object();
+            }
+
+            Status("Performing classic search...");
+
+            if (Proteins.Any())
+            {
+                int maxThreadsPerFile = CommonParameters.MaxThreadsToUsePerFile;
+                int[] threads = Enumerable.Range(0, maxThreadsPerFile).ToArray();
+                Parallel.ForEach(threads, (i) =>
+                {
+                    var peptideTheorProducts = new List<Product>();
+
+                    for (; i < Proteins.Count; i += maxThreadsPerFile)
+                    {
+                        // Stop loop if canceled
+                        if (GlobalVariables.StopLoops) { return; }
+
+                        // digest each protein into peptides and search for each peptide in all spectra within precursor mass tolerance
+                        foreach (PeptideWithSetModifications peptide in Proteins[i].Digest(CommonParameters.DigestionParams, FixedModifications, VariableModifications, SilacLabels, TurnoverLabels))
+                        {
+                            peptide.Fragment(CommonParameters.DissociationType, CommonParameters.DigestionParams.FragmentationTerminus, peptideTheorProducts);
+
+                            foreach (ScanWithIndexAndNotchInfo scan in GetAcceptableScans(peptide.MonoisotopicMass, SearchMode))
+                            {
+                                List<MatchedFragmentIon> matchedIons = MatchFragmentIons(scan.TheScan, peptideTheorProducts, CommonParameters);
+
+                                double thisScore = CalculatePeptideScore(scan.TheScan.TheScan, matchedIons);
+
+                                bool meetsScoreCutoff = thisScore >= CommonParameters.ScoreCutoff;
+
+                                // this is thread-safe because even if the score improves from another thread writing to this PSM,
+                                // the lock combined with AddOrReplace method will ensure thread safety
+                                if (meetsScoreCutoff)
+                                {
+                                    // valid hit (met the cutoff score); lock the scan to prevent other threads from accessing it
+                                    lock (myLocks[scan.ScanIndex])
+                                    {
+                                        bool scoreImprovement = PeptideSpectralMatches[scan.ScanIndex] == null || (thisScore - PeptideSpectralMatches[scan.ScanIndex].RunnerUpScore) > -PeptideSpectralMatch.ToleranceForScoreDifferentiation;
+
+                                        if (scoreImprovement)
+                                        {
+                                            if (PeptideSpectralMatches[scan.ScanIndex] == null)
+                                            {
+                                                PeptideSpectralMatches[scan.ScanIndex] = new PeptideSpectralMatch(peptide, scan.Notch, thisScore, scan.ScanIndex, scan.TheScan, CommonParameters, matchedIons, 0);
+                                            }
+                                            else
+                                            {
+                                                PeptideSpectralMatches[scan.ScanIndex].AddOrReplace(peptide, thisScore, scan.Notch, CommonParameters.ReportAllAmbiguity, matchedIons, 0);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        // report search progress (proteins searched so far out of total proteins in database)
+                        proteinsSearched++;
+                        var percentProgress = (int)((proteinsSearched / Proteins.Count) * 100);
+
+                        if (percentProgress > oldPercentProgress)
+                        {
+                            oldPercentProgress = percentProgress;
+                            ReportProgress(new ProgressEventArgs(percentProgress, "Performing classic search... ", NestedIds));
+                        }
+                    }
+                });
+            }
+
+            foreach (PeptideSpectralMatch psm in PeptideSpectralMatches.Where(p => p != null))
+            {
+                psm.ResolveAllAmbiguities();
+            }
+
+            return new MetaMorpheusEngineResults(this);
+        }
+
+        private IEnumerable<ScanWithIndexAndNotchInfo> GetAcceptableScans(double peptideMonoisotopicMass, MassDiffAcceptor searchMode)
+        {
+            foreach (AllowedIntervalWithNotch allowedIntervalWithNotch in searchMode.GetAllowedPrecursorMassIntervalsFromTheoreticalMass(peptideMonoisotopicMass).ToList())
+            {
+                DoubleRange allowedInterval = allowedIntervalWithNotch.AllowedInterval;
+                int scanIndex = GetFirstScanWithMassOverOrEqual(allowedInterval.Minimum);
+                if (scanIndex < ArrayOfSortedMS2Scans.Length)
+                {
+                    var scanMass = MyScanPrecursorMasses[scanIndex];
+                    while (scanMass <= allowedInterval.Maximum)
+                    {
+                        var scan = ArrayOfSortedMS2Scans[scanIndex];
+                        yield return new ScanWithIndexAndNotchInfo(scan, allowedIntervalWithNotch.Notch, scanIndex);
+                        scanIndex++;
+                        if (scanIndex == ArrayOfSortedMS2Scans.Length)
+                        {
+                            break;
+                        }
+
+                        scanMass = MyScanPrecursorMasses[scanIndex];
+                    }
+                }
+            }
+        }
+
+        private int GetFirstScanWithMassOverOrEqual(double minimum)
+        {
+            int index = Array.BinarySearch(MyScanPrecursorMasses, minimum);
+            if (index < 0)
+            {
+                index = ~index;
+            }
+
+            // index of the first element that is larger than value
+            return index;
+        }
+    }
+}
---
+++ metamorpheus_module/Test/CalibrationTests.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using TaskLayer;
+
+namespace Test
+{
+    internal class CalibrationTests
+    {
+        [Test]
+        [TestCase("filename1.mzML")]
+        [TestCase("filename1.1.mzML")]
+        public static void ExperimentalDesignCalibrationTest(string originalFileName)
+        {
+            CalibrationTask calibrationTask = new CalibrationTask();
+            string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestCalibration");
+            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\" + originalFileName);
+
+            File.Copy(Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallCalibratible_Yeast.mzML"), myFile, true);
+
+            string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\smalldb.fasta");
+            string experimentalDesignFilePath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\ExperimentalDesign.tsv");
+
+            Directory.CreateDirectory(outputFolder);
+
+            using (StreamWriter output = new StreamWriter(experimentalDesignFilePath))
+            {
+                output.WriteLine("FileName\tCondition\tBiorep\tFraction\tTechrep");
+                output.WriteLine(Path.GetFileNameWithoutExtension(myFile) + "\t" + "condition" + "\t" + "1" + "\t" + "1" + "\t" + "1");
+            }
+
+            calibrationTask.RunTask(outputFolder, new List<DbForTask> { new DbForTask(myDatabase, false) }, new List<string> { myFile }, "test");
+
+            var expDesignPath = Path.Combine(outputFolder, @"ExperimentalDesign.tsv");
+            var expDesign = File.ReadAllLines(expDesignPath);
+            string expectedCalibratedFileName = Path.GetFileNameWithoutExtension(myFile) + "-calib.mzML";
+            string expectedTomlName = Path.GetFileNameWithoutExtension(myFile) + "-calib.toml";
+
+            Assert.That(expDesign[1].Contains(Path.GetFileNameWithoutExtension(expectedCalibratedFileName)));
+            Assert.That(File.Exists(Path.Combine(outputFolder, myFile)));
+            Assert.That(File.Exists(Path.Combine(outputFolder, expectedTomlName)));
+
+            var lines = File.ReadAllLines(Path.Combine(outputFolder, expectedTomlName));
+            var tolerance = Regex.Match(lines[0], @"\d+\.\d*").Value;
+            var tolerance1 = Regex.Match(lines[1], @"\d+\.\d*").Value;
+
+            Assert.That(double.TryParse(tolerance, out double tol) == true);
+            Assert.That(double.TryParse(tolerance1, out double tol1) == true);
+            Assert.That(lines[0].Contains("PrecursorMassTolerance"));
+            Assert.That(lines[1].Contains("ProductMassTolerance"));
+
+            File.Delete(experimentalDesignFilePath);
+            File.Delete(myFile);
+            Directory.Delete(outputFolder, true);
+            Directory.Delete(Path.Combine(TestContext.CurrentContext.TestDirectory, @"Task Settings"), true);
+        }
+
+        [Test]
+        public static void CalibrationTestLowRes()
+        {
+            CalibrationTask calibrationTask = new CalibrationTask();
+
+            EngineLayer.CommonParameters CommonParameters = new EngineLayer.CommonParameters
+                (dissociationType: MassSpectrometry.DissociationType.LowCID,
+                scoreCutoff: 1);
+
+            string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestCalibrationLow");
+            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\sliced_b6.mzML");
+            string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\LowResSnip_B6_mouse_11700_117500pruned.xml");
+            Directory.CreateDirectory(outputFolder);
+
+            calibrationTask.RunTask(outputFolder, new List<DbForTask> { new DbForTask(myDatabase, false) }, new List<string> { myFile }, "test");
+            Assert.That(File.Exists(Path.Combine(outputFolder, @"sliced_b6-calib.mzML")));
+            Assert.That(File.Exists(Path.Combine(outputFolder, @"sliced_b6-calib.toml")));
+            var lines = File.ReadAllLines(Path.Combine(outputFolder, @"sliced_b6-calib.toml"));
+            var tolerance = Regex.Match(lines[0], @"\d+\.\d*").Value;
+            var tolerance1 = Regex.Match(lines[1], @"\d+\.\d*").Value;
+            Assert.That(double.TryParse(tolerance, out double tol) == true);
+            Assert.That(double.TryParse(tolerance1, out double tol1) == true);
+            Assert.That(lines[0].Contains("PrecursorMassTolerance"));
+            Assert.That(lines[1].Contains("ProductMassTolerance"));
+            Directory.Delete(outputFolder, true);
+            Directory.Delete(Path.Combine(TestContext.CurrentContext.TestDirectory, @"Task Settings"), true);
+        }
+    }
+}
