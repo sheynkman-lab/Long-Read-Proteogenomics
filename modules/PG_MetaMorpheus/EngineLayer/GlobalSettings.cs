7ade9e8d96f1163e8b9bf6c31e857be7b36493a4
rob <rmillikin@wisc.edu>
2020-11-08 17:11:03 -0600

    accept thermo license by default

--- modules/PG_MetaMorpheus/EngineLayer/GlobalSettings.cs
+++ modules/PG_MetaMorpheus/EngineLayer/GlobalSettings.cs
@@ -3,6 +3,6 @@
     public class GlobalSettings
     {
         public bool WriteExcelCompatibleTSVs { get; set; } = true;
-        public bool UserHasAgreedToThermoRawFileReaderLicence { get; set; } = false;
+        public bool UserHasAgreedToThermoRawFileReaderLicence { get; set; } = true; // TODO: change this into a CMD flag so Docker users can accept
     }
 }
