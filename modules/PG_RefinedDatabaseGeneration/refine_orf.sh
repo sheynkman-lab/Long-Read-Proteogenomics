421584d2125ea5d8db2c567bc688a92e2d11f46c
bj8th <bj8th@virginia.edu>
2020-12-08 10:22:35 -0500

    refine database readme

--- modules/PG_RefinedDatabaseGeneration/refine_orf.sh
+++ modules/PG_RefinedDatabaseGeneration/refine_orf.sh
@@ -1,8 +1,8 @@
 python refine_orf.py \
--io /mnt/shared/ubuntu/session_data/data/output/jurcat_cpat.ORF_called.tsv \
--is /mnt/shared/ubuntu/session_data/data/input/jurkat_corrected.fasta \
--or /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_redundant_accessions.txt \
--oct /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_orf_combined.tsv \
--ocf /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_orf_combined.fasta \
--ot /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_orf_aggregated.tsv \
--of /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_orf_aggregated.fasta
+--orfs /mnt/shared/ubuntu/session_data/data/output/jurcat_cpat.ORF_called.tsv \
+--pb_fasta /mnt/shared/ubuntu/session_data/data/input/jurkat_corrected.fasta \
+--redundant /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_redundant_accessions.txt \
+--combined_tsv /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_orf_combined.tsv \
+--combined_fasta /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_orf_combined.fasta \
+--agg_tsv /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_orf_aggregated.tsv \
+--agg_fasta /mnt/shared/ubuntu/session_data/data/refine/output/jurkat_orf_aggregated.fasta
