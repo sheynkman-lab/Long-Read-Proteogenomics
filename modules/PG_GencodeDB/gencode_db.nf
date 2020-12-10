a5087cc47e7067566b7690660b2ed29df9e43042
bj8th <bj8th@virginia.edu>
2020-12-10 17:30:32 -0500

    updated code to have main. added nextflow to pg_gencodedb and pg_makepacbiocdsgtf

---
+++ modules/PG_GencodeDB/gencode_db.nf
@@ -0,0 +1,76 @@
+#!/usr/bin/env nextflow
+/*
+ * Copyright (c) 2020, Sheynkman Lab and the authors.
+ *
+ *   This file is part of 'proteogenomics-nf' a pipeline repository to run
+ *   Gloria Sheynkman, Mike Shortreed and author's proteogenomics pipeline.
+ *
+ *   In this portion of the pipeline, LR_ORFCalling, the open reading frames are
+ *   called, analyzed and filtered.
+ *
+ * @authors
+ * Gloria Sheynkman
+ * Ben Jordan
+ * Anne Deslattes Mays (adeslat@scitechcon.org)
+ */
+
+def helpMessage() {
+    log.info """
+    Usage:
+    The typical command for running the pipeline is as follows:
+    nextflow run refined_db_generation.nf --orfs orf-testset-fraciton16.csv --seq jurkat_corrected.fasta --sample 'jurkat'
+    
+    Input files:
+      --gencode_fasta          path to gencode fasta file
+
+    Other:
+
+      --max_cpus                    Maximum number of CPUs (int)
+      --max_memory                  Maximum memory (memory unit)
+      --max_time                    Maximum time (time unit)
+
+    See here for more info: https://github.com/sheynkman-lab/Long-Read-Proteogenomics/blob/master/docs/usage.md
+    """.stripIndent()
+}
+
+// Show help message
+if (params.help) {
+  helpMessage()
+  exit 0
+}
+
+log.info "pg_gencode_db - N F  ~  version 0.1"
+log.info "====================================="
+log.info "gencode_fasta : ${params.gencode_fasta}"
+
+
+
+  /*--------------------------------------------------
+    Refined Protein Database Generation 
+  ---------------------------------------------------*/
+  Channel
+     .value(file(params.gencode_fasta))
+     .ifEmpty { error "Cannot find gencode fasta file for parameter --gencode_fasta: ${params.gencode_fasta}" }
+     .set { ch_gencode_fasta }   
+  
+  
+  process make_gencode_database {
+    tag "${gencode_fasta}"
+
+    publishDir "${params.outdir}/gencode_db/", mode: 'copy'
+
+    input:
+    file(gencode_fasta) from ch_gencode_fasta
+    
+    output:
+    file("*")
+    
+    script:
+    """
+    make_gencode_database.py \
+    --gencode_fasta $gencode_fasta \
+    --output_fasta gencode.fasta \
+    --output_cluster gencode_isoname_clusters.tsv
+    """
+  }
+
--- modules/PG_GencodeDB/make_gencode_database.py
+++ modules/PG_GencodeDB/make_gencode_database.py
@@ -5,25 +5,40 @@
 import pandas as pd
 from collections import defaultdict
 from Bio import SeqIO
+import argparse
 
 
-# make gencode clusters of same-protein-sequence entries
-# evaluate protein sequence similarity within the same gene
-gc = defaultdict(lambda: defaultdict(list)) # prot_seq -> gene -> [isonames]
-for rec in SeqIO.parse('../../data/gencode.v35.pc_translations.fa', 'fasta'):
-    gene = rec.id.split('|')[6]
-    isoname = rec.id.split('|')[5]
-    gc[rec.seq][gene].append(isoname)
 
 
-# write a fasta, with same-protein sequences clustered
-# for each group of same-protein-sequence isonames, choose the one 
-with open('gencode.fasta', 'w') as ofile, open('gencode_isoname_clusters.tsv', 'w') as ofile2:
-    ofile2.write('repr_isoname\tsame_prot_seq_isonames\n')
-    for seq in gc:
-        for gene, isonames in gc[seq].items():
-            isonames = sorted(isonames)
-            repr_isoname = isonames[0]
-            ofile.write('>gc|{}| GN={}\n{}\n'.format(repr_isoname, gene, seq))
-            ofile2.write(repr_isoname + '\t' + ','.join(isonames[1:]) + '\n')
+def make_gencode_database(gencode_fasta, ouptut_fasta, output_cluster):
+    # make gencode clusters of same-protein-sequence entries
+    # evaluate protein sequence similarity within the same gene
+    gc = defaultdict(lambda: defaultdict(list)) # prot_seq -> gene -> [isonames]
+    for rec in SeqIO.parse(gencode_fasta, 'fasta'):
+        gene = rec.id.split('|')[6]
+        isoname = rec.id.split('|')[5]
+        gc[rec.seq][gene].append(isoname)
+
+    # write a fasta, with same-protein sequences clustered
+    # for each group of same-protein-sequence isonames, choose the one 
+    with open(output_fasta, 'w') as ofile, open(output_cluster, 'w') as ofile2:
+        ofile2.write('repr_isoname\tsame_prot_seq_isonames\n')
+        for seq in gc:
+            for gene, isonames in gc[seq].items():
+                isonames = sorted(isonames)
+                repr_isoname = isonames[0]
+                ofile.write('>gc|{}| GN={}\n{}\n'.format(repr_isoname, gene, seq))
+                ofile2.write(repr_isoname + '\t' + ','.join(isonames[1:]) + '\n')
+    
+
+
+def main():
+    parser = argparse.ArgumentParser("Makes Gencode Clusters of same protein sequence entries")
+    parser.add_argument('--gencode_fasta', '-if', action='store', dest='gencode_fasta', help = 'gencode fasta file to group')
+    parser.add_argument('--output_fasta', '-of', action='store',dest='output_fasta', help = 'output file location of fasta file')
+    parser.add_argument('--output_cluster', '-oc', action='store', dest='output_cluster', 'output cluster tsv file location')
+    gencode_fasta = results.gencode_fasta
+    output_fasta = results.output_fasta
+    output_cluster = results.output_cluster
+    make_gencode_database(gencode_fasta, output_fasta, output_cluster)
 
