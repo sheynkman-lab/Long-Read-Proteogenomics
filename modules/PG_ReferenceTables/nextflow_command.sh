56013ce29b182514b2053dd771cddfc6cff0ca80
bj8th <bj8th@virginia.edu>
2020-12-14 23:08:26 +0000

    nextflow works on reference tables

---
+++ modules/PG_ReferenceTables/nextflow_command.sh
@@ -0,0 +1,3 @@
+nextflow ref_table_generation.nf \
+--gencode_gtf /mnt/shared/ubuntu/session_data/data/test_data/gencode.v35.annotation.chr22.gtf \
+--gencode_fasta /mnt/shared/ubuntu/session_data/data/gencode.v35.pc_transcripts.fa
