08e28e88c711b6bd62ee8c2620c243257d159f9d
Ben <bj8th@virginia.edu>
2021-04-27 08:17:25 -0400

    cloudos jurkat

--- modules/cpat/nextflow_command.sh
+++ modules/cpat/nextflow_command.sh
@@ -1,57 +1,10 @@
 DATA_DIR=/Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/data
-MIN_ORF=20
-nextflow cpat.nf \
---name jurkat_${MIN_ORF} \
---hexamer $DATA_DIR/input/Human_Hexamer.tsv \
---logit_model $DATA_DIR/input/Human_logitModel.RData \
---sample_fasta /Users/bj8th/Documents/Sheynkman-Lab/GitHub/LRPG-Manuscript/data/results/sqanti3-filtered/filtered_jurkat_corrected.chr1.fasta \
---min_orf $MIN_ORF \
---outdir results
-
 
 MIN_ORF=30
 nextflow cpat.nf \
 --name jurkat_${MIN_ORF} \
 --hexamer $DATA_DIR/input/Human_Hexamer.tsv \
 --logit_model $DATA_DIR/input/Human_logitModel.RData \
---sample_fasta /Users/bj8th/Documents/Sheynkman-Lab/GitHub/LRPG-Manuscript/data/results/sqanti3-filtered/filtered_jurkat_corrected.chr1.fasta \
+--sample_fasta /Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/modules/filter_sqanti/src/jurkat_corrected.5degfilter.fasta \
 --min_orf $MIN_ORF \
 --outdir results
-
-MIN_ORF=40
-nextflow cpat.nf \
---name jurkat_${MIN_ORF} \
---hexamer $DATA_DIR/input/Human_Hexamer.tsv \
---logit_model $DATA_DIR/input/Human_logitModel.RData \
---sample_fasta /Users/bj8th/Documents/Sheynkman-Lab/GitHub/LRPG-Manuscript/data/results/sqanti3-filtered/filtered_jurkat_corrected.chr1.fasta \
---min_orf $MIN_ORF \
---outdir results
-
-MIN_ORF=50
-nextflow cpat.nf \
---name jurkat_${MIN_ORF} \
---hexamer $DATA_DIR/input/Human_Hexamer.tsv \
---logit_model $DATA_DIR/input/Human_logitModel.RData \
---sample_fasta /Users/bj8th/Documents/Sheynkman-Lab/GitHub/LRPG-Manuscript/data/results/sqanti3-filtered/filtered_jurkat_corrected.chr1.fasta \
---min_orf $MIN_ORF \
---outdir results
-
-MIN_ORF=75
-nextflow cpat.nf \
---name jurkat_${MIN_ORF} \
---hexamer $DATA_DIR/input/Human_Hexamer.tsv \
---logit_model $DATA_DIR/input/Human_logitModel.RData \
---sample_fasta /Users/bj8th/Documents/Sheynkman-Lab/GitHub/LRPG-Manuscript/data/results/sqanti3-filtered/filtered_jurkat_corrected.chr1.fasta \
---min_orf $MIN_ORF \
---outdir results
-
-
-DATA_DIR=/Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/data
-MIN_ORF=30
-nextflow cpat.nf \
---name jurkat_${MIN_ORF} \
---hexamer $DATA_DIR/input/Human_Hexamer.tsv \
---logit_model $DATA_DIR/input/Human_logitModel.RData \
---sample_fasta /Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/modules/filter_sqanti/src/jurkat_chr1.collapsed.fasta \
---min_orf $MIN_ORF \
---outdir results_chr1
--- modules/visualization_track/run_track_visualizaiton_min_orf.sh
+++ modules/visualization_track/run_track_visualizaiton_min_orf.sh
@@ -1,7 +1,7 @@
 RESULTS_DIR=/Users/bj8th/Documents/Sheynkman-Lab/GitHub/LRPG-Manuscript/data/results
 REF_DIR=/Users/bj8th/Documents/Sheynkman-Lab/GitHub/LRPG-Manuscript/data/input
-BEST_ORF_DIR=/Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/modules/orf_calling/src/results_chr1
-REFINED_DIR=/Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/modules/refine_orf_database/src/results_chr1
+BEST_ORF_DIR=/Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/modules/orf_calling/src/results
+REFINED_DIR=/Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/modules/refine_orf_database/src/results
 
 # for MIN_ORF in 20 30 40 50 75
 # do
@@ -9,14 +9,14 @@ MIN_ORF=30
 nextflow run track_visualization.nf \
 --name jurkat_${MIN_ORF} \
 --include_transcript yes \
---sample_gtf /Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/modules/filter_sqanti/src/jurkat.chr1_corrected.5degfilter.gff \
+--sample_gtf /Users/bj8th/Documents/Sheynkman-Lab/GitHub/Long-Read-Proteogenomics/modules/filter_sqanti/src/jurkat_corrected.5degfilter.gff \
 --refined_info $REFINED_DIR/jurkat_${MIN_ORF}_orf_refined.tsv \
 --best_orf $BEST_ORF_DIR/jurkat_${MIN_ORF}_best_orf.tsv \
 --pb_gene $RESULTS_DIR/transcriptome_summary/pb_gene.tsv \
 --refined_fasta $REFINED_DIR/jurkat_${MIN_ORF}_orf_refined.fasta \
 --peptides $RESULTS_DIR/metamorpheus/pacbio/search_results/Task1SearchTask/AllPeptides.jurkat.psmtsv \
---gencode_gtf $REF_DIR/gencode.v35.annotation.chr1.gtf \
---outdir ./results_chr1 
+--gencode_gtf $REF_DIR/gencode.v35.annotation.gtf \
+--outdir ./results
 # done
 
 
